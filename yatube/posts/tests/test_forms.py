595d5a1d3083a4be067176a05067a0ba5c02d031
Олейник Павел <oleiip@yandex.com>
2022-09-01 12:27:02 +0300

    test_forms

---
+++ yatube/posts/tests/test_forms.py
@@ -0,0 +1,71 @@
+import shutil
+import tempfile
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+from posts.models import Group, Post
+User = get_user_model()
+
+
+class PostCreateForm(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # Создаем запись в базе данных для проверки сушествующего slug
+        cls.user = User.objects.create_user(username='author')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='test-slug',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            text='Текст поста',
+            author=cls.user,
+            group=cls.group,
+            pub_date='Дата публикации',
+        )
+        cls.form = PostCreateForm()
+
+    def setUp(self):
+        self.guest_client = Client()
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
+
+
+    def test_create_post(self):
+        posts_count = Post.objects.count()
+        form_data = {
+            'group': PostCreateForm.group.id,
+            'text': 'Тестовый текст',
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True
+        )
+        self.assertRedirects(
+            response,
+            reverse('posts:profile', kwargs={'username': 'author'}))
+
+        self.assertEqual(Post.objects.count(), posts_count + 1)
+        self.assertTrue(
+            Post.objects.filter(
+                text='Тестовый текст',
+                group=PostCreateForm.group,
+            ).exists()
+        )
+
+    def test_cant_create_existing_slug(self):
+        post_count = Post.objects.count()
+        form_data = {
+            'title': 'Тестовая группа',
+            'description': 'Тестовое описание',
+            'slug': 'test-slug'
+        }
+        response = self.guest_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True
+        )
+        self.assertEqual(Post.objects.count(), post_count)
+        self.assertEqual(response.status_code, 200)
